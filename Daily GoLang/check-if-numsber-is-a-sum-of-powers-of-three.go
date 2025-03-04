0a12eb5a0da79d418adedbf089e1908a57d5d715
Nikita Nikolayuk <113298204+minimaledit@users.noreply.github.com>
2025-03-04 16:43:38 +0300

    Create check-if-numsber-is-a-sum-of-powers-of-three.go

---
+++ Daily GoLang/check-if-numsber-is-a-sum-of-powers-of-three.go
@@ -0,0 +1,20 @@
+/*
+Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+An integer y is a power of three if there exists an integer x such that y == 3^x.
+Example: Input: n = 12   Output: true
+Explanation: 12 = 3^1 + 3^2
+*/
+// The number can not be represented as a sum of powers of 3 if it's ternary presentation has a 2 in it
+func checkPowersOfThree(n int) bool {
+    var ost int
+    flag := true
+    for n!=0 {
+        ost = n % 3
+        if ost == 2{
+            flag = false
+            n = 0
+        }
+        n /= 3
+    }
+    return flag
+}
