2861438b55ceae6cbbce54b72fbb6120e63da88a
Nikita Nikolayuk <113298204+minimaledit@users.noreply.github.com>
2025-03-16 00:41:02 +0300

    Update ti-diamond.go

--- SomeStuff/ti-diamond.go
+++ SomeStuff/ti-diamond.go
@@ -22,3 +22,28 @@ func printDiamond(num int) {
 		fmt.Printf("\n")
 	}
 }
+
+//Без лишних итераций используя лейбл
+func printDiamond(num int) {
+	fmt.Println("Мой бриллиант:")
+ligma:
+	for i := 0; i < num*2-1; i++ {
+		for j := 0; j < num*2-1; j++ {
+			switch {
+			case i < num && j >= num-1 && i == j-num+1:
+				fmt.Printf("#\n")
+				continue ligma
+			case i < num && j < num && i+j+1 == num:
+				fmt.Printf("#")
+			case i >= num-1 && j >= num-1 && i+j == 3*(num-1):
+				fmt.Printf("#\n")
+				continue ligma
+			case i >= num && j < num && i-num+1 == j:
+				fmt.Printf("#")
+			default:
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
